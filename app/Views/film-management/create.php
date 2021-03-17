065fcf4ed7cdb1e0382c9154f3137b465fba135b
AhmadFarid <ahmadfarid3991@gmail.com>
2021-03-17 16:22:11 +0800

    update

---
+++ app/Views/film-management/create.php
@@ -0,0 +1,67 @@
+<?php
+echo form_open('film-management/store',['class'=>'form-control']);
+?>
+  <div class="form-group row">
+    <?php 
+        echo form_label('Title','title',['class'=>'col-sm-2 col-form-label']);
+    ?>
+    <div class="col-sm-10">
+      <?php
+        echo form_input('title','',['class'=>'form-control','placeholder'=>'Insert title']);
+      ?>
+
+    </div>
+  </div>
+
+  <div class="form-group row">
+     <?php 
+        echo form_label('Description','description',['class'=>'col-sm-2 col-form-label']);
+    ?>
+    <div class="col-sm-10">
+        <?php
+            echo form_textarea('description','',['class'=>'form-control','placeholder'=>'Insert description']);
+        ?>
+    </div>
+  </div>
+
+  <div class="form-group row">
+     <?php 
+        echo form_label('Release Year','release_year',['class'=>'col-sm-2 col-form-label']);
+    ?>
+    <div class="col-sm-10">
+        <?php
+            echo form_input('release_year','',['class'=>'form-control','placeholder'=>'Insert Release Year'],'number');
+        ?>
+    </div>
+  </div>
+
+  <div class="form-group row">
+     <?php 
+        echo form_label('Language','language_id',['class'=>'col-sm-2 col-form-label']);
+    ?>
+    <div class="col-sm-10">
+        <?php
+            echo form_dropdown('language_id','','',['class'=>'form-control','placeholder'=>'Insert Language']);
+        ?>
+    </div>
+  </div>
+
+  <div class="form-group row">
+     <?php 
+        echo form_label('Rating','rating',['class'=>'col-sm-2 col-form-label']);
+    ?>
+    <div class="col-sm-10">
+        <?php
+            echo form_dropdown('rating','','',['class'=>'form-control','placeholder'=>'Insert Rating']);
+        ?>
+    </div>
+  </div>
+  
+  <div class="form-group row">
+    <div class="col-sm-10">
+      <button type="submit" class="btn btn-primary">Create</button>
+    </div>
+  </div>
+<?php
+echo form_close();
+?>
---
+++ app/Views/film-management/index.php
@@ -0,0 +1,70 @@
+<?php
+$rentalajax=site_url('/filmmanagement/ajaxfilmdt');
+
+$session = session();
+$keepsearch=$session->get('keepsearch');
+// print_r($keepsearch);
+?>
+
+<script>
+$(document).ready(function() {
+
+<?php
+if($keepsearch !=1){
+?>
+var table=$('#example').DataTable();
+ table.state.clear();
+ table.destroy();
+<?php
+}else{
+
+    $session->remove('keepsearch');
+}
+?>
+
+    $('#example').DataTable( {
+        "processing": true,
+        "serverSide": true,
+        'stateSave':true,
+        "ajax": "<?=$rentalajax?>",
+        columnDefs: [
+            
+        { targets:0, orderable:false},    
+        { targets:1, name:'title'},
+        { targets:2, name:'description' },
+        { targets:3, name:'release_year' },
+        { targets:4, name:'l.name' },
+        { targets:5, name:'rating' },
+        { targets:6, orderable:false},   
+    ]
+    } );
+} );
+</script>
+
+<a class="btn btn-success" href="<?= site_url('film-management/create') ?>" role="button">Create</a>
+<br><br>
+
+<table id="example" class="display" style="width:100%">
+        <thead>
+            <tr>
+                <th>NO</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Release Year</th>
+                <th>Language</th>
+                <th>Rating</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <th>NO</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Release Year</th>
+                <th>Language</th>
+                <th>Rating</th>
+                <th>Action</th>
+            </tr>
+        </tfoot>
+    </table>
